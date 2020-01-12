5e11eb9f00c3914c2263bd538c359bedaa3cc034
es279 <emirsonzogni279@gmail.com>
2020-01-12 05:54:51 -0300

    Obtención del último id de usuario de la BD

--- app/src/main/java/utn/aplicaciones/riquelmito/CrearCuentaActivity.java
+++ app/src/main/java/utn/aplicaciones/riquelmito/CrearCuentaActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -18,6 +19,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -41,7 +43,7 @@ public class CrearCuentaActivity extends AppCompatActivity {
 
     private List<Integer> listIdUsuario = new ArrayList<Integer>();
     ArrayAdapter<Integer> arrayAdapterIdUsuario;
-    private Integer id;
+    private Integer id = 99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +57,21 @@ public class CrearCuentaActivity extends AppCompatActivity {
         etSingUpPasswordRe = (EditText) findViewById(R.id.etSingUpPasswordRe);
 
         inicializarFirebase();
+
+        databaseReference.child("IdUsuario").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    id = Integer.parseInt( dataSnapshot.child("valor").getValue().toString() );
+                    Log.d("CREATION","********** "+id);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     public void crearCuentaClick(View view){
@@ -96,27 +113,8 @@ public class CrearCuentaActivity extends AppCompatActivity {
         if(operacionValida){
             //Si no hubieron errores
             final Context cont = this;
-            //TODO autogenerar idPostulante
-            databaseReference.child("IdUsuario").addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    listIdUsuario.clear();
-                    for(DataSnapshot objSnapshot : dataSnapshot.getChildren()){
-                        id = objSnapshot.getValue(Integer.class);
-                        //listIdUsuario.add(id);
-                        Toast.makeText(cont, id.toString(),Toast.LENGTH_LONG).show();
-                    }
 
-
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                }
-            });
-
-            Usuario usuario = new Usuario(id, etSingUpEmail.getText().toString(), etSingUpPassword.getText().toString(), null, null, null, null, null, null, null, null, null, null, null, null, null);
+            Usuario usuario = new Usuario(id+1, etSingUpEmail.getText().toString(), etSingUpPassword.getText().toString(), null, null, null, null, null, null, null, null, null, null, null, null, null);
             //TODO agregar valores ingresados por el ususario
             registrarUsuario(usuario);
         }
@@ -134,6 +132,10 @@ public class CrearCuentaActivity extends AppCompatActivity {
             AlertDialog dialog = builder.create();
             dialog.show();
         }
+    }
+
+    private Integer getUltimoIdUsuarioDB(){
+        return  null;
     }
 
     private boolean esEmailValido(String email){
@@ -156,6 +158,8 @@ public class CrearCuentaActivity extends AppCompatActivity {
 
     private void registrarUsuario(Usuario usuario){
         //TODO Añadir el nuevo usuario a la base de datos
+
+        Log.d("CREATION","********** jarrito café");
         databaseReference.child("Usuario").child(usuario.getIdPostulante().toString()).setValue(usuario);
     }
 
